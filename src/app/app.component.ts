a1e72c032c3d13707b4a0a8f718ec54c6eff823a
Rafal <rgrajcar@gmail.com>
2024-09-16 22:24:51 +0200

    added parent component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloComponent} from "./hello/hello.component";
+import {ParentComponent} from "./parent/parent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent],
+  imports: [RouterOutlet, HelloComponent, ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
