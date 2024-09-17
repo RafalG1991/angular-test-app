4365ffd39a23d3aeb514ba0a40e5aa44d43be53e
Rafal <rgrajcar@gmail.com>
2024-09-17 20:30:59 +0200

    added ngIf directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloComponent} from "./hello/hello.component";
 import {ParentComponent} from "./parent/parent.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent, ParentComponent],
+  imports: [RouterOutlet, HelloComponent, ParentComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TestApp';
   users: string[] = ['A', 'B', 'C'];
+  shouldBeVisible=true;
 }
