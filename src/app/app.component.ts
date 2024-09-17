77772ba13b77d72e7dfe1da93350a11a06d18c40
Rafal <rgrajcar@gmail.com>
2024-09-17 20:35:37 +0200

    added ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloComponent} from "./hello/hello.component";
 import {ParentComponent} from "./parent/parent.component";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponent, ParentComponent, NgIf],
+  imports: [RouterOutlet, HelloComponent, ParentComponent, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TestApp';
-  users: string[] = ['A', 'B', 'C'];
+  users: string[] = ['A', 'B', 'C', 'D', 'E'];
   shouldBeVisible=true;
 }
