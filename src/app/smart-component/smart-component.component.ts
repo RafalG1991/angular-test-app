1c7424edbefcdfb995b275fd17edf6ab1c63352c
Rafal <rgrajcar@gmail.com>
2024-09-23 21:48:41 +0200

    added new smart-component

---
+++ src/app/smart-component/smart-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './smart-component.component.html',
+  styleUrl: './smart-component.component.scss'
+})
+export class SmartComponentComponent {
+
+}
