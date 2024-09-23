9233940583eb6b228b7b7be858ccc73785eafbd4
Rafal <rgrajcar@gmail.com>
2024-09-23 21:40:12 +0200

    added new conditional-content component

---
+++ src/app/conditional-content/conditional-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conditional-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './conditional-content.component.html',
+  styleUrl: './conditional-content.component.scss'
+})
+export class ConditionalContentComponent {
+
+}
