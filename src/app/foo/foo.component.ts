52a3cc3902122107572bbbe576b3422d406f071c
Rafal <rgrajcar@gmail.com>
2024-09-24 16:05:58 +0200

    added interface OnInit to foo component and ngOnInit function

--- src/app/foo/foo.component.ts
+++ src/app/foo/foo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-foo',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './foo.component.html',
   styleUrl: './foo.component.scss'
 })
-export class FooComponent {
-
+export class FooComponent implements OnInit{
+  ngOnInit() {
+    console.log("onInit");
+}
 }
