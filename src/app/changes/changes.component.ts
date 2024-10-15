d8bda5660ada43540ac4fddd3c462026ac881257
Rafal <rgrajcar@gmail.com>
2024-10-15 18:32:34 +0200

    added value property as a string and displayed it in paragraph in changes component

--- src/app/changes/changes.component.ts
+++ src/app/changes/changes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-changes',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './changes.component.scss'
 })
 export class ChangesComponent {
-
+  @Input()
+  value: string = '';
 }
