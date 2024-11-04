9246274dd0c886b4abcb82c01355fb543767306a
Rafal <rgrajcar@gmail.com>
2024-11-04 21:26:49 +0100

    added take pipe to subscribe 5 values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import {FooComponent} from "./foo/foo.component";
 import {TemplateComponent} from "./template/template.component";
 import {ChildComponent} from "./child/child.component";
 import {ChangesComponent} from "./changes/changes.component";
-import {filter, from, observable, Observable, of} from "rxjs";
+import {filter, from, observable, Observable, of, take} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -102,11 +102,23 @@ export class AppComponent implements OnInit {
   obsValue!: Observable<number>;
 
   ngOnInit(): void {
-    const obs1$ = of([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    const obs2$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const counter = new Observable<number>(observer => {
+      let counter = 0;
+      setInterval(() => {
+        observer.next(counter++);
+      }, 1000);
+    })
 
-    obs1$.subscribe(val => console.log("OF", val));
-    obs2$.subscribe(val => console.log("FROM", val));
+    counter
+      .pipe(
+        take(5),
+      )
+      .subscribe(val => console.log(val));
+    // const obs1$ = of([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    // const obs2$ = from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    //
+    // obs1$.subscribe(val => console.log("OF", val));
+    // obs2$.subscribe(val => console.log("FROM", val));
 
     // this.obsValue = new Observable<number>(observer => {
     //   // observer.next(1000);
