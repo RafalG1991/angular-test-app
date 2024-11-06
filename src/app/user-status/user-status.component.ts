07d4c6e2848abd27a7897618d4a9fc8c5596a266
Rafal <rgrajcar@gmail.com>
2024-11-06 20:53:36 +0100

    added new UserStatus component to display authentication status depends on isAuth value from auth service

---
+++ src/app/user-status/user-status.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth.service";
+
+@Component({
+  selector: 'app-user-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-status.component.html',
+  styleUrl: './user-status.component.scss'
+})
+export class UserStatusComponent implements OnInit{
+  status: string = '';
+
+  constructor(private authService: AuthService) {};
+
+  ngOnInit() {
+    const isAuth = this.authService.isAuthenticated;
+
+    this.status = isAuth ? 'Logged in' : 'Not logged in';
+  }
+}
