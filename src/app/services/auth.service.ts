7fae1aeef69bdbb1c005357e7c5274daf4160b02
Rafal <rgrajcar@gmail.com>
2024-11-06 21:44:16 +0100

    added BehaviorSubject in AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,26 @@
 import {Injectable, signal} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public isAuthenticated = signal<boolean>(false);
+  public isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
   login() {
     this.isAuthenticated.set(true);
   }
 
+  loginSubject() {
+    this.isAuthenticatedSubject.next(true);
+  }
+
   logout() {
     this.isAuthenticated.set(false);
+  }
+
+  logoutSubject() {
+    this.isAuthenticatedSubject.next(false);
   }
 }
