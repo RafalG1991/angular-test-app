39381fcf0208eac6fb875ea743018df831ca697d
Rafal <rgrajcar@gmail.com>
2024-11-15 19:50:50 +0100

    added retry method in pipe to retry 3 times to fetch for data

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {retry} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,9 @@ export class ApiService {
   private http = inject(HttpClient);
 
   get<R>(url: string) {
-    return this.http.get<R>(url);
+    return this.http.get<R>(url)
+      .pipe(
+        retry(3)
+      );
   }
 }
