92ad6d8b870512dea05416873d0cdceedd042655
Rafal <rgrajcar@gmail.com>
2024-11-15 19:55:50 +0100

    added catchError to print error in console and pass new generic error to component

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {retry} from "rxjs";
+import {catchError, retry, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,12 @@ export class ApiService {
   get<R>(url: string) {
     return this.http.get<R>(url)
       .pipe(
-        retry(3)
+        retry(3),
+        catchError(err => {
+          console.log(err);
+
+          return throwError(() => new Error('Something went wrong! Try again later!'));
+        })
       );
   }
 }
