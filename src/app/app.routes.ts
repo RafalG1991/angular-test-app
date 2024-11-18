a0bcfe58ee1fbd093aa031b6480c0087e2261a32
Rafal <rgrajcar@gmail.com>
2024-11-18 21:40:29 +0100

    added new components to /product/list and /product/:productId paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ProductsListComponent} from "./products-list/products-list.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'product/list', component: ProductsListComponent },
+  { path: 'product/:productId', component: ProductDetailsComponent },
 ];
