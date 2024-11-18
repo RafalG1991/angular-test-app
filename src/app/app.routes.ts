4945dcc67d59e722bbdb09cdcc4a98e4a05d96aa
Rafal <rgrajcar@gmail.com>
2024-11-18 21:42:57 +0100

    added new /product path with nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import {HomeComponent} from "./home/home.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {ProductsListComponent} from "./products-list/products-list.component";
 import {ProductDetailsComponent} from "./product-details/product-details.component";
+import {ProductComponent} from "./product/product.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'product/list', component: ProductsListComponent },
-  { path: 'product/:productId', component: ProductDetailsComponent },
+  { path: 'product',  component: ProductComponent, children: [
+      { path: 'product/list', component: ProductsListComponent },
+      { path: 'product/:productId', component: ProductDetailsComponent },
+    ]
+  },
 ];
