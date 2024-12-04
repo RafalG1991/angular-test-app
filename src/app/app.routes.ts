a4d773d85974f021e0ebb30866f0e83194919450
Rafal <rgrajcar@gmail.com>
2024-12-04 20:08:32 +0100

    added PokemonList and PokemonDetails components and added them to router on proper paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,28 @@ import {ProductDetailsComponent} from "./product-details/product-details.compone
 import {ProductComponent} from "./product/product.component";
 import {ErrorComponent} from "./error/error.component";
 import {authGuard} from "./auth.guard";
+import {PokemonListComponent} from "./pokemon-list/pokemon-list.component";
+import {PokemonDetailsComponent} from "./pokemon-details/pokemon-details.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  {
-    path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component')
-      .then(c => c.DashboardComponent),
-  },
-  {
-    path: 'product',
-    canActivate: [authGuard],
-    component: ProductComponent,
-    children: [
-      { path: 'list', component: ProductsListComponent },
-      { path: ':productId', component: ProductDetailsComponent },
-    ]
-  },
+  { path: 'pokemons', component: PokemonListComponent },
+  { path: 'pokemons/:id', component: PokemonDetailsComponent },
+
+  // {
+  //   path: 'dashboard',
+  //   loadComponent: () => import('./dashboard/dashboard.component')
+  //     .then(c => c.DashboardComponent),
+  // },
+  // {
+  //   path: 'product',
+  //   canActivate: [authGuard],
+  //   component: ProductComponent,
+  //   children: [
+  //     { path: 'list', component: ProductsListComponent },
+  //     { path: ':productId', component: ProductDetailsComponent },
+  //   ]
+  // },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: ErrorComponent },
 ];
