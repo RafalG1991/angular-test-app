5718666afb57c72edd91d4e57686cd99cf447c0c
Rafal <rgrajcar@gmail.com>
2024-12-04 21:38:30 +0100

    added service to get pokemon details from api in pokemon details component

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {PokemonService} from "../services/pokemon.service";
+import {filter, map, Observable, switchMap} from "rxjs";
+import {PokemonDetailsResponse, PokemonListElement} from "../types";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-pokemon-details',
@@ -7,6 +11,17 @@ import { Component } from '@angular/core';
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.scss'
 })
-export class PokemonDetailsComponent {
+export class PokemonDetailsComponent implements OnInit {
+  private pokemonService = inject(PokemonService);
+  private route = inject(ActivatedRoute);
 
+  pokemon$!: Observable<PokemonDetailsResponse>;
+
+  ngOnInit() {
+    this.route.paramMap.pipe(
+      map(params => params.get('id')),
+      filter(pokemonName => pokemonName !== null),
+      switchMap(pokemonName => this.pokemonService.getPokemonDetail(pokemonName))
+    )
+  }
 }
