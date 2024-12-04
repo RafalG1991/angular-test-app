2d0ec4a53beca20d54c37b415cf283c2ca05d223
Rafal <rgrajcar@gmail.com>
2024-12-04 20:25:26 +0100

    added pokemons list to template file

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -2,11 +2,14 @@ import {Component, inject, OnInit} from '@angular/core';
 import {PokemonService} from "../services/pokemon.service";
 import {map, Observable} from "rxjs";
 import {PokemonListElement} from "../types";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss'
 })
