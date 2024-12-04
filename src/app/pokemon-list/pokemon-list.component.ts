a339011eeb46889dbfaba9f3b45bf87cf3d43ec3
Rafal <rgrajcar@gmail.com>
2024-12-04 20:23:25 +0100

    added onInit to get the pokemon list from api after component mount

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {PokemonService} from "../services/pokemon.service";
+import {map, Observable} from "rxjs";
+import {PokemonListElement} from "../types";
 
 @Component({
   selector: 'app-pokemon-list',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss'
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit{
+  private pokemonService = inject(PokemonService);
+
+  pokemons$!: Observable<PokemonListElement[]>;
+
+  ngOnInit() {
+    this.pokemons$ = this.pokemonService.getPokemon()
+      .pipe(
+        map(response => response!.results),
+      )
+  }
 
 }
