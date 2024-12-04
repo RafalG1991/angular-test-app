f377a2a340e84cea6ae38321d1296a4d237944ff
Rafal <rgrajcar@gmail.com>
2024-12-04 20:26:35 +0100

    added new singlePokemon component

---
+++ src/app/single-pokemon/single-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-pokemon.component.html',
+  styleUrl: './single-pokemon.component.scss'
+})
+export class SinglePokemonComponent {
+
+}
