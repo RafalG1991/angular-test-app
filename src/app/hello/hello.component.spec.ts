eb1c2eee210ef01edee18ed770355b7e805a2d76
Rafal <rgrajcar@gmail.com>
2024-12-16 21:06:42 +0100

    added new test for message signal display

--- src/app/hello/hello.component.spec.ts
+++ src/app/hello/hello.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloComponent } from './hello.component';
+import {By} from "@angular/platform-browser";
 
 describe('HelloComponent', () => {
   let component: HelloComponent;
@@ -37,5 +38,16 @@ describe('HelloComponent', () => {
     const elementP = fixture.nativeElement.querySelector('p');
 
     expect(elementP.textContent).toBe(expectedText);
+  });
+
+  it('should render paragraph with message signal', () => {
+    const expectedText = 'hello!';
+
+    fixture.componentRef.setInput('message', 'hello!');
+    fixture.detectChanges();
+
+    const elementP = fixture.debugElement.query(By.css('[data-testid="welcome-test"]'));
+
+    expect(elementP.nativeElement.textContent).toBe(expectedText);
   });
 });
